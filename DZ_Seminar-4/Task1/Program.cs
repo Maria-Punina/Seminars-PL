0c650d4064e525ee4da5e45498ec34a42047a026
Maria Punina <puninama@mail.ru>
2023-11-05 01:08:07 +0500

    1

---
+++ DZ_Seminar-4/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵) 
+// 2, 4 -> 16
+
+// 1. Cоздать цикл приема чисел А и Б 
+// 2. Использовать метод Math.Pow(A,Б) для возведения числа А в натуральную степень B 
+
+
+void numberAB(double a , double b)
+{
+    
+    while(a < 10 && b < 10)
+    {
+        double result = Math.Pow(a,b);
+        Console.WriteLine($" {a} ^ {b} = {result}");
+        a++;
+        b++;
+    }
+  
+}
+
+Console.Write("Введите число A : ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число B : ");
+double y = Convert.ToDouble(Console.ReadLine());
+numberAB(x,y);
---
+++ DZ_Seminar-4/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+// 1. Создадим случайное число
+// 2. Разложим наше число на десятки сотни тысячи .... 
+// 3. Сложим все элементы 
+// 4. Выводим результат в терминал
+
+int Promt(string message)         
+{
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput); //int.Parse аналог Convert.ToInt32
+    return result;
+}
+
+int SumAllNumbers(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {                                                                                                                                           // 3
+        result+= number % 10; 
+        number = number / 10;    
+    }
+    return result; 
+}
+int number = Promt("Введи число: ");
+Console.WriteLine($"Сумма всех чисел в цифре {number} = {SumAllNumbers(number)}"); 
